1ef5a901ca6204d116b69486609f7b575ebcc622
aritramullick <aritramullick@g.ucla.edu>
2023-04-22 15:35:50 -0700

    Inital

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { useRef, useState } from 'react';
-
+import LoginForm from "./LoginForm";
 
 const WebSocketComponent = () => {
   const [socket, setSocket] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [receivedFile, setReceivedFile] = useState(null);
   let selectedFilename = useRef('');
+  let currUsername = useRef('');
+  let currPassword = useRef('');
   
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
@@ -15,27 +17,17 @@ const WebSocketComponent = () => {
 
   const handleWebSocketOpen = () => {
     console.log('WebSocket connection opened');
+    let initObject = {	
+      requestType: "Init",
+      username: currUsername.current,
+      password: currPassword.current
+    }
+    socket.send(JSON.stringify(initObject));
   };
 
   const handleWebSocketMessage = (event) => {
     const receivedData = JSON.parse(event.data);
-
-    // Assume the server sends back an object with a "type" field that indicates the file type
-    if (receivedData.type === 'text') {
-      const receivedText = receivedData.content;
-      const blob = new Blob([receivedText], { type: 'text/plain' });
-      setReceivedFile(blob);
-    } else if (receivedData.type === 'image') {
-      const receivedImage = receivedData.content;
-      const blob = new Blob([receivedImage], { type: 'image/jpeg' });
-      setReceivedFile(blob);
-    } else if (receivedData.type === 'audio') {
-      const receivedAudio = receivedData.content;
-      const blob = new Blob([receivedAudio], { type: 'audio/mpeg' });
-      setReceivedFile(blob);
-    } else {
-      console.error('Received unknown file type:', receivedData.type);
-    }
+    console.log("Response from server:", receivedData);
   };
 
   const handleWebSocketClose = () => {
@@ -64,38 +56,42 @@ const WebSocketComponent = () => {
     let base64FileContent = patelFunction(selectedFile);
 
     let saveFileObject = {
+      username: "",
+      password: "",
       requestType: "saveFile",
       name: selectedFilename.current,
       body: base64FileContent
     };
 
     socket.send(JSON.stringify(saveFileObject));
- 
+    selectedFilename.current = "";
   };
 
   const handleConnectButtonClick = () => {
     const newSocket = new WebSocket('ws://localhost:8080');
-
     newSocket.addEventListener('open', handleWebSocketOpen);
     newSocket.addEventListener('message', handleWebSocketMessage);
     newSocket.addEventListener('close', handleWebSocketClose);
-    
+
     setSocket(newSocket);
+  };
+
+  const handleLogin = ({ username, password }) => {
+    console.log("Login data:", { username, password });
+    currUsername.current = username;
+    currPassword.current = password;
   };
 
   return (
     <div>
+      <div>
+      <h1>Login Form</h1>
+      <LoginForm onLogin={handleLogin} />
+    </div>
       <button onClick={handleConnectButtonClick}>Initialize Connection</button>
       <input type="file" onChange={handleFileInputChange} />
       <button onClick={handleSendButtonClick}>Send File</button>
-      {receivedFile && (
-        <div>
-          <p>Received file:</p>
-          {receivedFile.type.startsWith('image') && <img src={URL.createObjectURL(receivedFile)} />}
-          {receivedFile.type.startsWith('audio') && <audio controls src={URL.createObjectURL(receivedFile)} />}
-          {receivedFile.type === 'text/plain' && <pre>{receivedFile.text()}</pre>}
-        </div>
-      )}
+   
     </div>
   );
 };
